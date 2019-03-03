c6d9ec016d539c80544012623853f019b14e6220
M416 <chanon_w@cmu.ac.th>
2019-03-03 14:35:31 +0700

    implement test recycle

--- app/src/main/java/com/example/m/mproject499/Fragment/TestFragment.kt
+++ app/src/main/java/com/example/m/mproject499/Fragment/TestFragment.kt
@@ -3,9 +3,14 @@ package com.example.m.mproject499
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.m.mproject499.Adapter.DaysAdapter
+import com.example.m.mproject499.Adapter.TestAdapter
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_stat.*
 import kotlinx.android.synthetic.main.fragment_test.*
 
 class TestFragment : Fragment() {
@@ -27,7 +32,24 @@ class TestFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         test_page.text = "TEST FRAGMENT"
+
+        val adapter = activity?.applicationContext?.let { TestAdapter(it) }
+        val layoutManager = LinearLayoutManager(activity?.applicationContext)
+        test_recycle?.layoutManager = layoutManager
+        test_recycle?.adapter = adapter
+        adapter?.loadData(generateDataTest())
+        adapter?.notifyDataSetChanged()
+
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun generateDataTest(): ArrayList<String> {
+        val result = ArrayList<String>()
+
+        for (i in 1..6) {
+            result.add(i.toString())
+        }
+        return result
     }
 
 
