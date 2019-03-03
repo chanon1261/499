5e84d8a6be175f18e8350800fa37c90840385f13
M416 <chanon_w@cmu.ac.th>
2019-03-03 14:38:56 +0700

    fix

--- app/src/main/java/com/example/m/mproject499/Fragment/TestFragment.kt
+++ app/src/main/java/com/example/m/mproject499/Fragment/TestFragment.kt
@@ -7,10 +7,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.m.mproject499.Adapter.DaysAdapter
 import com.example.m.mproject499.Adapter.TestAdapter
-import kotlinx.android.synthetic.main.fragment_main.*
-import kotlinx.android.synthetic.main.fragment_stat.*
 import kotlinx.android.synthetic.main.fragment_test.*
 
 class TestFragment : Fragment() {
