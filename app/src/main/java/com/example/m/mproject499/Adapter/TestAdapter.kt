ab24e16148c73c151776b37ab985f301c269c76b
M416 <chanon_w@cmu.ac.th>
2019-03-03 17:43:20 +0700

    fix code

--- app/src/main/java/com/example/m/mproject499/Adapter/TestAdapter.kt
+++ app/src/main/java/com/example/m/mproject499/Adapter/TestAdapter.kt
@@ -37,12 +37,18 @@ class TestAdapter(val context: Context): RecyclerView.Adapter<TestAdapter.TestAd
         fun bind(item: String) {
             binding.setVariable(BR.test_name, item)
             itemView.setOnClickListener {
-                context.toast("TEST")
-                MainApp.instance.applicationContext?.let { context  ->
-                    val intent = SpeakingActivity.getStartIntent(context)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    intent.putExtra("key","${adapterPosition+1}")
-                    context.startActivity(intent)
+                itemView.setOnClickListener {
+                    if(adapterPosition == 0 || adapterPosition == 1){
+                        context.toast("LISTENING")
+                        doStartActivity(ListeningActivity.getStartIntent(context))
+
+                    }else if(adapterPosition == 2 || adapterPosition == 3){
+                        context.toast("MATCHING")
+                        doStartActivity(MatchingActivity.getStartIntent(context))
+                    }else {
+                        context.toast("SPEAKING")
+                        doStartActivity(SpeakingActivity.getStartIntent(context))
+                    }
                 }
             }
 
@@ -51,7 +57,7 @@ class TestAdapter(val context: Context): RecyclerView.Adapter<TestAdapter.TestAd
         private fun doStartActivity(intent:Intent) {
             MainApp.instance.applicationContext?.let { context ->
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                intent.putExtra("key", "${position + 1}")
+                intent.putExtra("key", "${adapterPosition + 1}")
                 context.startActivity(intent)
             }
         }
