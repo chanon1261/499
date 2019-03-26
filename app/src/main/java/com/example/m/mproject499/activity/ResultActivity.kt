4c85b85c1bdb9bcb0c70321e3c920712fc00944f
M416 <chanon_w@cmu.ac.th>
2019-03-26 14:19:39 +0700

    bug find value

--- app/src/main/java/com/example/m/mproject499/activity/ResultActivity.kt
+++ app/src/main/java/com/example/m/mproject499/activity/ResultActivity.kt
@@ -10,9 +10,19 @@ import com.example.m.mproject499.MainApp.Companion.History
 import com.example.m.mproject499.MainApp.Companion.Result
 import com.example.m.mproject499.R
 import com.example.m.mproject499.adapter.ResultAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_result.*
+import com.google.firebase.database.DataSnapshot
+import android.util.Log
+import com.example.m.mproject499.model.Score
+import java.util.HashMap
+
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     companion object {
         fun getStartIntent(context: Context) = Intent(context, ResultActivity::class.java)
@@ -35,6 +45,39 @@ class ResultActivity : AppCompatActivity() {
         ListResult?.adapter = adapter
         adapter.loadData(History, Result)
         adapter.notifyDataSetChanged()
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+
+        val uid = auth.currentUser!!.uid
+
+        database.child("users-score").orderByChild("uid")
+            .equalTo(uid)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+
+                    Log.d("fxfx", "======= ERROR")
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+
+                    for (postSnapshot in p0.children) {
+//                        Log.d("fxfx", "=======" + postSnapshot.child("email").value!!)
+//                        Log.d("fxfx", "=======" + postSnapshot.child("username").value!!)
+//                        if ((postSnapshot.child("status").value!! as Boolean)) {
+//                            val score1: Int = postSnapshot.child("listening").value!! as Int
+//                            writeNewScore(uid, (score + score1) / 2.0, 0.0, 0.0, true)
+//                        } else {
+//                            writeNewScore(uid, score.toDouble(), 0.0, 0.0, true)
+//                        }
+                        writeNewScore(uid, score.toDouble(), 0.0, 0.0, true)
+                    }
+
+                }
+
+            })
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -46,5 +89,20 @@ class ResultActivity : AppCompatActivity() {
     override fun onDestroy() {
         Result.clear()
         super.onDestroy()
+    }
+
+    private fun writeNewScore(userId: String, listening: Double, speaking: Double, matching: Double, status: Boolean) {
+        val score = Score(userId, listening, speaking, matching, status)
+        val childUpdates = HashMap<String, Any>()
+        childUpdates["/users-score/$userId"] = score.toMap()
+        database.updateChildren(childUpdates)
+    }
+
+    private fun usernameFromEmail(email: String): String {
+        return if (email.contains("@")) {
+            email.split("@".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
+        } else {
+            email
+        }
     }
 }
--- app/src/main/java/com/example/m/mproject499/model/User.kt
+++ app/src/main/java/com/example/m/mproject499/model/User.kt
@@ -1,7 +1,24 @@
 package com.example.m.mproject499.model
 
+import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class User(var username: String = "", var email: String = "")
 
+
+@IgnoreExtraProperties
+data class Score(var uid: String = "", var listening: Double = 0.0,var speaking: Double = 0.0,var matching: Double = 0.0,var status: Boolean)
+{
+
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "uid" to uid,
+            "listening" to listening,
+            "speaking" to speaking,
+            "matching" to matching,
+            "status" to status
+        )
+    }
+}
